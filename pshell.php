acf6e6f8285ab419ddda85ad0ce00e336b411e97
zhaolei254 <zhaolei254@vip.qq.com>
2010-05-19 10:48:23 +0000

    a

--- pshell.php
+++ pshell.php
@@ -6,22 +6,28 @@ define('SBUG', $argv[2]);
 
 //get from bat argv
 $shell_name = $argv[1];
-$shell_title = $argv[2];
+$cmd_char = '#';
 $br = chr(10);
 
 //shell var init
-$shell_str_arr = array();
+$shell_str_arr = array("echo 'Hello';");
+$eval_str = '';
 
-while(shell_v($shell_str_arr)) {
+while($eval_str = shell_v($shell_str_arr)) {
+	
+	(SBUG)?eval($eval_str):@eval($eval_str);
+	
+	echo $br;
 	
 	//arr init
 	unset($shell_str_arr);
 	$shell_str_arr = array();
 	
-	echo $shell_name.'#';
+	echo $shell_name.$cmd_char;
 	$cmd_str = '';
 
-	$cmd_str = trim(fgets(STDIN)); 
+	$cmd_str = trim(fgets(STDIN));
+	$cmd_str = addslashes($cmd_str);
 	$str_len = strlen($cmd_str);
 	
 	
@@ -36,21 +42,23 @@ while(shell_v($shell_str_arr)) {
 			$shell_str_arr[] = $cmd_str;
 			
 			$cmd_str = trim(fgets(STDIN));
+			//$cmd_str = addslashes($cmd_str);
 			$str_len = strlen($cmd_str);
 		}
 	} else {
 		($cmd_str[$str_len-1] == ';') ? $cmd_str : $cmd_str .= ';';
-			
+
 		$shell_str_arr[] = $cmd_str;
 	}
-	
+
 	SBUG&&print($cmd_str.$br);
 }
 
-
+//echo '::'.$a;
 
 function shell_v($shell_str_arr) {
 	$quit_arr = array('exit', 'exit;', 'quit', 'quit;');
+	$cmd_arr = array('help', 'set_char');
 	
 	if(empty($shell_str_arr)) {
 		return true;
@@ -64,19 +72,11 @@ function shell_v($shell_str_arr) {
 			}
 			
 			$cmd_str = implode("\n", $shell_str_arr);
-			
-			$str_len = strlen($cmd_str);
-			//$cmd_str = substr($cmd_str, 0, $str_len-2);
-			SBUG&&print($cmd_str.$br);
-			
-			eval($cmd_str);
-			
-			echo chr(10);
+
+			return $cmd_str;		
 			
 			return true;
 		} catch (Exception $e) {
-			echo 'bug';
-			print_r($e);
 			exit(1);
 		}
 	}
--- t.php
+++ t.php
@@ -1,5 +1,12 @@
 <?php
 	$a = 10;
-	eval('$d=1000;');
-	eval('print $d;');
+	//$str = '$a=1000;';
+	$str = trim(fgets(STDIN));
+	//eval($str);
+	//$str = addslashes($str); 
+	eval($str);
+	echo 'e:'.$a.chr(10);
+	
+	$cmd_str = trim(fgets(STDIN));
+	eval($cmd_str);
 
